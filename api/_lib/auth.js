783fd23ced29cc88b454ee52177b42781b7f2b80
Andik_dyy <139764520+andre3122@users.noreply.github.com>
2025-08-17 17:17:31 +0700

    Create auth.js

---
+++ api/_lib/auth.js
@@ -0,0 +1,17 @@
+api/
+  _lib/
+    auth.js          ← auth helper (yang kamu tanya)
+    telegram.js
+    db.js
+  setup/
+    migrate.js
+  user/
+    get.js
+  checkin/
+    claim.js
+  reward/
+    complete.js
+  referrals.js
+  address/
+    save.js
+  withdraw.js
