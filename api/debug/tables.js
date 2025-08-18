48a7e47edb99c553b8f3974dac02629b0c6d548e
Andik_dyy <139764520+andre3122@users.noreply.github.com>
2025-08-19 02:45:30 +0700

    Delete api/debug/tables.js

--- api/debug/tables.js
+++
@@ -1,7 +0,0 @@
-const { sql } = require("../_lib/db");
-module.exports = async (req, res) => {
-  const { rows } = await sql`
-    SELECT table_name FROM information_schema.tables
-    WHERE table_schema='public' ORDER BY table_name`;
-  res.json(rows.map(r => r.table_name));
-};
